710578a5a348093bc8a332a6caf3ab6b26b3e680
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2023-10-31 17:23:08 -0300

    77

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,9 +1,10 @@
 import User from '#/utils/models/user'
 import {Router} from 'express'
+import { CreateUser } from '#/@types/user'
 
 const router = Router()
 
-router.post("/create", async (req, res) => {
+router.post("/create", async (req: CreateUser, res) => {
 
    const {email, password, name} = req.body
    //const newUser = new User({email, password, name})
