83201ad60b1f3b2f1c4a512eb2f85e5f2ae31a4a
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2023-10-31 17:38:59 -0300

    76

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,17 +1,32 @@
-import User from '#/utils/models/user'
-import {Router} from 'express'
-import { CreateUser } from '#/@types/user'
+import User from '#/utils/models/user';
+import { Router, Request, Response, NextFunction } from 'express';
+import { CreateUser } from '#/@types/user';
 
-const router = Router()
+const router = Router();
 
-router.post("/create", async (req: CreateUser, res) => {
+router.post("/create",
+    (req: Request, res: Response, next: NextFunction) => {
+        const { name } = req.body;
+        
+        if (!name || !name.trim()) {
+            return res.status(400).json({ error: 'Name is missing!' });
+        }
+        
+        if (name.length < 3) {
+            return res.status(400).json({ error: 'Invalid name! Name should be at least 3 characters long.' });
+        }
+        
+        next();
+    },
+    async (req: Request & { body: CreateUser }, res: Response) => {
+        try {
+            const { email, password, name } = req.body;
+            const user = await User.create({ name, email, password });
+            res.status(201).json({ user }); // 201 means "Created"
+        } catch (error) {
+            res.status(500).json({ error: "There was an error creating the user." });
+        }
+    }
+);
 
-   const {email, password, name} = req.body
-   //const newUser = new User({email, password, name})
-   //newUser.save()
-
-   const user = await User.create({name, email, password})
-   res.json({user})
-})
-
-export default router
+export default router;
