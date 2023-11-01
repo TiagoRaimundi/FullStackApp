52dd32b7412371804b17baca1248ac1e7b169af5
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2023-11-01 17:39:39 -0300

    85

---
+++ src/models/emailVerificationToken.ts
@@ -0,0 +1,30 @@
+//interface (typescript)
+import { Model, ObjectId, Schema, model} from 'mongoose';
+
+interface EmailVerificationTokenDocument {
+    owner: ObjectId
+    token: string
+    createdAt: Date
+}
+
+const emailVerificationTokenSchema = new 
+Schema<EmailVerificationTokenDocument>(
+    {
+        owner: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: "User"
+        },
+        token: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            expires: 3600, //60 min = 3600s
+            default: Date.now()
+        }
+
+    })
+
+export default model("EmailVerificationToken", emailVerificationTokenSchema) as Model<EmailVerificationTokenDocument>
