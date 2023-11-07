911b344e6b6d700612d5ccdd1a628b034e746478
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2023-11-07 15:10:23 -0300

    92

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,9 +1,10 @@
 
-import { CreateUser } from '#/@types/user';
+import { CreateUser, VerifyEmailRequest } from '#/@types/user';
 import { RequestHandler } from 'express';
 import User from '#/models/user';
 import { sendVerificationMail } from '#/utils/mail';
 import { generateToken } from '#/utils/helpers';
+import EmailVerificationToken from '#/models/emailVerificationToken';
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
     const {email,  password, name} = req.body
@@ -17,3 +18,30 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
     sendVerificationMail(token, {name, email, userId: user._id.toString() })
     res.status(201).json({user: {id: user._id, name, email}})
 }
+
+export const verifyEmail: RequestHandler = async (req: VerifyEmailRequest, res) => {
+  const {token, userId} = req.body
+
+  const verificationToken = await EmailVerificationToken.findOne({
+    owner: userId
+
+  })
+
+  if(!verificationToken) return res.status(403).json({error: "Invalid token!"})
+  
+  
+  const matched = await verificationToken.compareToken(token)
+  if(!matched) return res.status(403).json({error: "Invalid token!"})
+
+
+
+  await User.findByIdAndUpdate(userId, {
+    verified: true
+  })
+  
+  await EmailVerificationToken.findByIdAndDelete(verificationToken._id)
+  
+  res.json({message: "Your email is verified"})
+  
+}
+
--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -2,20 +2,12 @@ import User from '../models/user';
 import { Router, Request, Response } from 'express';
 import { CreateUserSchema } from '../utils/validationSchema';
 import { validate } from '#/middleware/validator';
-import { create } from '#/controllers/user';
+import { create, verifyEmail } from '#/controllers/user';
 
-const router = Router();
+const router = Router()
 
-router.post("/create", validate(CreateUserSchema),create, async (req, res) => {
-    
-    try {
-        // Assuming req.body has been validated and matches the CreateUser type
-        const user = await User.create(req.body);
-        res.status(201).json(user);
-    } catch (error) {
-        // If there's an error in the User.create operation, send an appropriate response
-        res.status(500).json({ error: 'There was an error creating the user.' });
-    }
-});
+router.post("/create", validate(CreateUserSchema),
+create)
+router.post("/verify-email", verifyEmail )
 
 export default router;
