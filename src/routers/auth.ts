3d9eb669394380c0a02a519007ab01097009f2f6
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2023-11-14 14:42:20 -0300

    109

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -8,8 +8,6 @@ import { JwtPayload, verify } from 'jsonwebtoken';
 import { JWT_SECRET } from '#/utils/variables';
 import User from '../models/user';
 
-
-
 const router = Router()
 
 router.post("/create", validate(CreateUserSchema),
@@ -20,17 +18,25 @@ router.post("/forget-password", generateForgetPasswordLink )
 router.post("/verify-pass-reset-token",validate(TokenAndIDValidation), isValidPassResetToken, grantValid)
 router.post('/update-password', validate(updatePasswordSchema), isValidPassResetToken, updatePassword)
 router.post('/sign-in', validate(SignInValidationSchema), signIn)
-router.get('/is-auth', mustAuth, (req, res)=>{
-    
+router.get('/is-auth', mustAuth, (req, res)=>{  
     res.json({
         profile: req.user,
      })
  
 })
 
+router.get('/public', (req, res)=>{  
+    res.json({
+        message: "You are in public route."
+     })
+ 
+})
 
-
-
-
+router.get('/private', mustAuth, (req, res)=>{  
+    res.json({
+        message: "You are in private route."
+     })
+ 
+})
   
 export default router;
