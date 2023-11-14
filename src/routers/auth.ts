e3baef46bf7637e4c298f1cda8be53aa85660822
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2023-11-14 16:09:06 -0300

    113

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -27,16 +27,31 @@ router.get('/is-auth', mustAuth, (req, res)=>{
 
 
 import formidable from 'formidable';
+import path from 'path'
+import fs from 'fs'
 
-router.post('/update-profile', (req, res) => {
+router.post('/update-profile', async (req, res) => {
     //handle the file upload
     if(!req.headers["content-type"]?.startsWith("multipart/form-data"))
         return res.status(422).json({error: "Only accepts form-data!"})
 
-    const form = formidable();
+        const dir = path.join(__dirname, "../public/profiles")
+
+        try {
+            await fs.readdirSync(dir)
+        } catch (error) {
+            await fs.mkdirSync(dir)
+        } 
+
+    const form = formidable({
+        uploadDir: dir,
+        filename(name, ext, part, form) {
+            return Date.now() + "_" + part.originalFilename
+        }
+    });
     form.parse(req, (err, fields, files) => {
-        console.log("fields: ", fields)
-        console.log("files: ", files)
+        //console.log("fields: ", fields)
+        //console.log("files: ", files)
 
         res.json({uploaded: true})
     })
