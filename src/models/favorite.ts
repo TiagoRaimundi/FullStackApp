3a694f5c958d29a1969c34dc944bdc280a9e840d
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2023-11-22 15:15:38 -0300

    128

--- src/models/favorite.ts
+++ src/models/favorite.ts
@@ -6,16 +6,15 @@ interface FavoriteDocument{
     items: ObjectId[]
 }
 
-const favoriteSchema = new Schema<FavoriteDocument>({
-    owner: {
+const favoriteSchema = new Schema<FavoriteDocument>(
+    {
         owner: {
             type: Schema.Types.ObjectId,
             ref: "User"
 
         },
         items: [{type: Schema.Types.ObjectId, ref: "Audio" }]
-    }
-}, {timestamps: true})
+    }, {timestamps: true})
 
 const Favorite = models.Favorite || model("Favorite", favoriteSchema );
 
