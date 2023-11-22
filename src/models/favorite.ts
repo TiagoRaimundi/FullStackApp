16de3eb693cebc91644106da16007fd70967a469
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2023-11-22 14:56:11 -0300

    125

---
+++ src/models/favorite.ts
@@ -0,0 +1,22 @@
+import { AudioValidationSchema } from "#/utils/validationSchema";
+import { Model, ObjectId, Schema, model, models } from "mongoose";
+
+interface FavoriteDocument{
+    owner: ObjectId
+    items: ObjectId[]
+}
+
+const favoriteSchema = new Schema<FavoriteDocument>({
+    owner: {
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+
+        },
+        items: [{type: Schema.Types.ObjectId, ref: "Audio" }]
+    }
+}, {timestamps: true})
+
+const Favorite = models.Favorite || model("Favorite", favoriteSchema );
+
+export default model("Favorite", favoriteSchema) as Model<FavoriteDocument>;
